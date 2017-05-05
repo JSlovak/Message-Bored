5b7e5df8a3a2c53f00e0a6a087fb42dfd2608649
JSlovak <jasmine.slovak@gmail.com>
2017-05-04 21:52:56 -1000

    Added all the routes, and need to help with checking if working using curl

--- api/messages/index.js
+++ api/messages/index.js
@@ -4,17 +4,40 @@ const express = require('express');
 const messages = express.Router();
 const { Message } = require('../../models');
 
-messages.get('/latest', (req, res)=> {
-  Message.all()
-    .then(( messages ) =>{
-      res.json(messages);
+messages.post('/', (req, res) => {
+  Message.create( {
+    author_id: req.body.author_id,
+    body: req.body.body,
+    topic_id: req.body.topic_id
+  })
+    .then( message => {
+      console.log(message);
+      res.json.bind(message);
+    })
+    .catch( err => {
+      res.json.bind(err);
     });
 });
 
-messages.post('/', (req, res) => {
-  Message.create( req.body )
-    .then( res.json.bind(res) )
-    .catch( res.json.bind(res) );
+messages.get('/latest', (req, res)=> {
+  Message.all({
+    include:[{
+      model: db.Topic,
+      attributes:['name']
+    },
+    {
+      model: db.User,
+      as: 'Author',
+      attributes: ['name']
+    }],
+    // order: ???
+    where: {
+      topic_id: req.params.topic_id
+    }
+  })
+    .then(( messages ) =>{
+      res.json(messages);
+    });
 });
 
 messages.get('/by-topic/:topic_id',(req, res) => {
