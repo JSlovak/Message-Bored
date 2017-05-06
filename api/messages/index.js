a28425f9b3b7ee29ac115c969b090b740a7ed62d
JSlovak <jasmine.slovak@gmail.com>
2017-05-05 19:40:31 -1000

    messages routes are working and set up

--- api/messages/index.js
+++ api/messages/index.js
@@ -3,48 +3,54 @@
 const express = require('express');
 const messages = express.Router();
 const { Message } = require('../../models');
+const LATEST_LIMIT = 10;
 
-messages.post('/', (req, res) => {
-  Message.create( {
-    author_id: req.body.author_id,
-    body: req.body.body,
-    topic_id: req.body.topic_id
-  })
-    .then( message => {
-      console.log(message);
-      res.json.bind(message);
-    })
-    .catch( err => {
-      res.json.bind(err);
-    });
-});
+// Postgres doesn't maintain an order, it will give you a random 10
 
-messages.get('/latest', (req, res)=> {
-  Message.all({
-    include:[{
-      model: db.Topic,
-      attributes:['name']
-    },
-    {
-      model: db.User,
-      as: 'Author',
-      attributes: ['name']
-    }],
-    // order: ???
-    where: {
-      topic_id: req.params.topic_id
-    }
-  })
-    .then(( messages ) =>{
-      res.json(messages);
-    });
-});
-
-messages.get('/by-topic/:topic_id',(req, res) => {
+messages.post('/', (req, res) =>
   Message.create( req.body )
     .then( res.json.bind(res) )
-    .catch( res.json.bind(res) );
+    .catch( res.json.bind(res) )
+);
+
+messages.get('/latest', (req,res) =>{
+  Message.all({
+    include: [
+      {
+        model: User,
+        as: 'Author'
+      },
+      {
+        model: Topic,
+        as: 'Topic'
+      }
+    ],
+    order: [
+      ['updatedAt', 'DESC']
+    ],
+    limit : LATEST_LIMIT
+  })
+  .then( res.json.bind(res) );
 });
 
+messages.get('/by-topic/:topic_id', ( req, res ) => {
+  Message.all({
+    include: [
+      {
+        model: User,
+        as: 'Author'
+      },
+      {
+        model: Topic,
+        as: 'Topic'
+      }
+    ],
+    order: [
+      ['createdAt', 'ASC']
+    ],
+    where : { topic_id : req.params.topic_id }
+  })
+  .then( res.json.bind(res) );
+});
 
 module.exports = messages;
